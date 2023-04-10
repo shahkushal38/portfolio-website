0d4fa0ac9fe583067fc0d4f27fef63d3122caca2
shahkushal38 <shahkushal38@gmail.com>
2023-04-10 16:18:55 +0530

    style added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Live Link - https://shahkushal38.github.io/portfolio-website/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/contact'
+const App = () => {
+  return (
+    <>
+    <About/>
+    <Experience/>
+    <Contact/>
+    </>
+  )
+}
+
+export default App
