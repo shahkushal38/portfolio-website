2aa6fc3a81f072739b944bbf29fb7db18d1fda59
shahkushal38 <shahkushal38@gmail.com>
2023-05-24 23:49:51 +0530

    added about and responsiveness

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -27,8 +27,8 @@ const About = () => {
 
             <article className='about_card'>
               <BsBookFill className='about__icon' />
-              <h5>Educatiom</h5>
-              <small>B.Tech in Infomration Technology Engineering</small>
+              <h5>Education</h5>
+              <small>B.Tech in Information Technology Engineering</small>
             </article>
 
             <article className='about_card'>
@@ -39,10 +39,9 @@ const About = () => {
           </div>
 
           <p>
-          I am a final year I.T. Engineering student pursuing B.Tech with majors in Information Technology Engineering from Sardar Patel Institute of Technology and a minor in Management from S.P. Jain Institute of Management and Research, Mumbai. I have always been passionate about the field of computer science. Over the past 2 years, I have gained professional experience in the IT industry through internships in software development and also working as a part-time software development programme instructor. I have gained valuable insights into the practical application of my academic knowledge. I have also participated and won various Hackathons, recently being Hack This Fall 3.0 by the MLH community. I have honed my skills in programming languages such as Java, Python, JavaScript and solidity and have worked on a range of technologies and frameworks such as Node.js, Express Js, Vue Js, Django, Flask and MongoDB. <br></br><br />
-
-          Currently, I am exploring the field of blockchain by participating in hackathons and seeking exposure to this cutting-edge technology with a keen interest in blockchain's potential to revolutionize various industries. I have been working to develop my skills in smart contract development and decentralized application (dApp) development and understanding various blockchain platforms such as Ethereum ERC tokens, Solana, Hyperledger Fabric and 5ire.
-
+          I have a B.Tech in Information Technology Engineering from Sardar Patel Institute of Technology along with a minor degree in Management from S.P. Jain Institute of Management and Research, Mumbai. I have always been passionate about the field of computer science. Over the past 2 years, I have gained professional hands-on experience in the IT industry through internships in software development, learning new technologies and building software projects resulting in gaining valuable insights into the practical application of my academic knowledge. I have also participated and won various Hackathons. <br></br><br />
+          
+          I have honed my skills in programming languages such as Core Java, Python, JavaScript and Solidity and have worked on a range of technologies and frameworks such as Node.js, Express.js, Vue.js, React.js, Django, Flask and databases in MongoDB (Non-Relational) and MySQL (Relational). 
           </p>
 
           <a href="#contact" className='btn btn-primary'> Let's Talk</a>
