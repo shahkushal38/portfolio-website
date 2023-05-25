466df2d34ff0aed05cac8d1d1999f81adfb308f4
shahkushal38 <shahkushal38@gmail.com>
2023-05-25 19:51:40 +0530

    added projects

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,120 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 const experience = () => {
   return (
-    <section id = "experience">experience</section>
+    <section id = "experience">
+      <h5>What Skills and Experiences I Have</h5>
+      <h2>My Technical Skills and Work Experiences</h2>
+      
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Front-End Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>VueJs</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>ReactJs</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END oF FRONT-END */}
+        <div className='experience__backend'>
+          <h3>Back-End Programming & Frameworks</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Core Java</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>NodeJs</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>ExpressJs</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Django</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Flask</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
-
+import './projects.css'
 const Projects = () => {
   return (
-    <section id = "projects">projects</section>
+    <section id = "projects">
+      <h5> See What I have Build</h5>
+      <h2>My Personal Projects</h2>
+
+      <div className='container project_container'>
+        <div className='project_1'>
+          <h3 className='project_title'> <b>KYC Verified CrowdFunding Solution </b></h3>
+          <div className='project_content'>
+            <p className='project_tech'><b>Blockchain:</b> Ethereum, Solidity, 5ireChain | <b>Front-End: </b> VueJs, JavaScript, HTML, CSS, Bootstrap | <b>Back-End:</b> Python, Flask | <b>Machine-Learning:</b> HaarCascades, DeepFace Face Verification model  </p>
+
+            <hr></hr>
+            <p>
+            <ul className='text-light' >
+              <li> 
+                Developed a sustainable hybrid model of decentralized crowdfunding application backed with centralised KYC system solving the problem of user trusts and abiding by government compliances and regulations, which was missing in traditional web3 blockchain application
+              </li>
+              <li>
+                Developed solidity smart contracts of an end to end crowdfunding application using ERC-20 token standards along with deploying the contracts on 5ireChain Testnet
+              </li>
+            </ul>
+            </p>
+            <div className='project_button'>
+            <a href="#contact" className='btn btn-primary'>Github Repository</a>
+            <a href="#contact" className='btn btn-primary'> Project Demo - YouTube </a>
+            </div>
+          </div>
+        </div>
+
+        <div className='project_1'>
+          <h3 className='project_title'> <b>KYC verified CrowdFunding Solution </b></h3>
+          <div className='project_content'>
+            <h4>Tech Stack - Blockchain: Ethereum, Solidity, 5ireChain | Front-End: VueJs, JavaScript, HTML, CSS, Bootstrap | Back-End: Python, Flask | Machine-Learning: HaarCascades, DeepFace Face Verification model  </h4>
+
+            <hr></hr>
+            <p>
+            <ul className='text-light' >
+              <li> 
+                Developed a sustainable hybrid model of decentralized crowdfunding application backed with centralised KYC system solving the problem of user trusts and abiding by government compliances and regulations, which was missing in traditional web3 blockchain application
+              </li>
+              <li>
+                Developed solidity smart contracts of an end to end crowdfunding application using ERC-20 token standards along with deploying the contracts on 5ireChain Testnet
+              </li>
+            </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    
+    </section>
   )
 }
 
