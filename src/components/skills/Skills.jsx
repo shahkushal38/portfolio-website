5b7be321c93b741cf51b530135e5621bb02b6a47
Kushal Shah <shahkushal38@gmail.com>
2026-02-21 16:52:49 +0530

    skills update

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -110,6 +110,43 @@ const Skills = () => {
             </article>
           </div>
         </div>
+        {/* END oF BACK-END */}
+        <div className='experience__frontend'>
+          <h3>Database and Tools</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className='text-light'>Advanced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Advanced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Oracle MySQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Git - Version Control</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+
+
       </div>
 
     </section>
