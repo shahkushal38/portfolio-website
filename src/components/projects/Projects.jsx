2ae2fb24c1117219569d7fd80ce53873b0a81918
Kushal Shah <60709835+shahkushal38@users.noreply.github.com>
2026-02-22 12:16:24 +0530

    Skills update (#2)
    * skills update
    
    * work update
    
    * skills update
    
    * remove space
    
    * project khabrichain
    
    * formatting
    
    * Khabrichain project
    
    * kyc project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 import './projects.css'
 const Projects = () => {
   return (
-    <section id = "projects">
+    <section id="projects">
       <h5> See What I have Build</h5>
       <h2>My Personal Projects</h2>
 
       <div className='container project_container'>
+        <div className='project_2'>
+          <h3 className='project_title'> <b>KhabriChain - Decentralized reporting system </b></h3>
+          <div className='project_content'>
+            <p className='project_tech'><b>Blockchain:</b> Ethereum, Solidity | <b>Front-End: </b> ReactJs, JavaScript, HTML, CSS, Bootstrap | <b>Back-End:</b> Python, Flask </p>
+            <hr></hr>
+            <p>
+              <ul className='text-light' >
+                <li>
+                  Designed and developed an intelligent and privacy preserving framework that allows users to anonymously report or tip any criminal activities.
+                </li>
+                <li>
+                  Developed a real-time crime detection system without compromising on user privacy using a combination of blockchain and 3rd party SDK integrations for Adhaar based user authentications.
+                </li>
+              </ul>
+              <div className='project_button'>
+              <a href="https://github.com/shahkushal38/khabrichain" className='btn btn-primary' target="_blank">Github Repository</a>
+              <a href="https://www.youtube.com/watch?v=oEM4TUQ87kw" className='btn btn-primary' target="_blank"> Project Demo - YouTube </a>
+              <a href="https://devfolio.co/projects/khabrichain-decentralized-reporting-system-7b59" className='btn btn-primary' target="_blank"> Devfolio </a>
+            </div>
+            </p>
+          </div>
+
+        </div>
+
         <div className='project_1'>
           <h3 className='project_title'> <b>KYC Verified CrowdFunding Solution </b></h3>
           <div className='project_content'>
@@ -14,42 +38,25 @@ const Projects = () => {
 
             <hr></hr>
             <p>
-            <ul className='text-light' >
-              <li> 
-                Developed a sustainable hybrid model of decentralized crowdfunding application backed with centralised KYC system solving the problem of user trusts and abiding by government compliances and regulations, which was missing in traditional web3 blockchain application
-              </li>
-              <li>
-                Developed solidity smart contracts of an end to end crowdfunding application using ERC-20 token standards along with deploying the contracts on 5ireChain Testnet
-              </li>
-            </ul>
+              <ul className='text-light' >
+                <li>
+                  Developed a sustainable hybrid model of decentralized crowdfunding application backed with centralised KYC system solving the problem of user trusts and abiding by government compliances and regulations, which was missing in traditional web3 blockchain application
+                </li>
+                <li>
+                  Developed solidity smart contracts of an end to end crowdfunding application using ERC-20 token standards along with deploying the contracts on 5ireChain Testnet
+                </li>
+              </ul>
             </p>
             <div className='project_button'>
-            <a href="#contact" className='btn btn-primary'>Github Repository</a>
-            <a href="#contact" className='btn btn-primary'> Project Demo - YouTube </a>
+              <a href="https://github.com/shahkushal38/secure-patrons-frontend" className='btn btn-primary' target="_blank">Github - Frontend</a>
+              <a href="https://github.com/shahkushal38/secure-patrons-backend" className='btn btn-primary' target="_blank">Github - Backend</a>
+              <a href="https://www.youtube.com/watch?v=95f56iuBGOA" className='btn btn-primary' target="_blank"> Project Demo - YouTube </a>
+              <a href="https://devpost.com/software/kyc-verified-crowdfunding-solution-secure-patrons" className='btn btn-primary' target="_blank"> Devfolio </a>
             </div>
           </div>
         </div>
-
-        <div className='project_1'>
-          <h3 className='project_title'> <b>KYC verified CrowdFunding Solution </b></h3>
-          <div className='project_content'>
-            <h4>Tech Stack - Blockchain: Ethereum, Solidity, 5ireChain | Front-End: VueJs, JavaScript, HTML, CSS, Bootstrap | Back-End: Python, Flask | Machine-Learning: HaarCascades, DeepFace Face Verification model  </h4>
-
-            <hr></hr>
-            <p>
-            <ul className='text-light' >
-              <li> 
-                Developed a sustainable hybrid model of decentralized crowdfunding application backed with centralised KYC system solving the problem of user trusts and abiding by government compliances and regulations, which was missing in traditional web3 blockchain application
-              </li>
-              <li>
-                Developed solidity smart contracts of an end to end crowdfunding application using ERC-20 token standards along with deploying the contracts on 5ireChain Testnet
-              </li>
-            </ul>
-            </p>
-          </div>
-        </div>
       </div>
-    
+
     </section>
   )
 }
