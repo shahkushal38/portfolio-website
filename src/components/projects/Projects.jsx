4ff3b470b4031559619fbad5b6f5afe2e86ea293
Kushal Shah <shahkushal38@gmail.com>
2026-02-22 12:06:29 +0530

    Khabrichain project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,18 +8,24 @@ const Projects = () => {
 
       <div className='container project_container'>
         <div className='project_2'>
-          <h3 className='project_title'> <b>KhabriChain - A Decentralised Tipping System </b></h3>
+          <h3 className='project_title'> <b>KhabriChain - Decentralized reporting system </b></h3>
           <div className='project_content'>
-            <h4>Tech Stack - Blockchain: Ethereum, Solidity | Front-End: ReactJs, JavaScript, HTML, CSS, Bootstrap | Back-End: Python, Flask </h4>
-
+            <p className='project_tech'><b>Blockchain:</b> Ethereum, Solidity | <b>Front-End: </b> ReactJs, JavaScript, HTML, CSS, Bootstrap | <b>Back-End:</b> Python, Flask </p>
             <hr></hr>
             <p>
               <ul className='text-light' >
                 <li>
-                  Developed a sustainable hybrid model of decentralized tipping application.
+                  Designed and developed an intelligent and privacy preserving framework that allows users to anonymously report or tip any criminal activities.
                 </li>
-
+                <li>
+                  Developed a real-time crime detection system without compromising on user privacy using a combination of blockchain and 3rd party SDK integrations for Adhaar based user authentications.
+                </li>
               </ul>
+              <div className='project_button'>
+              <a href="https://github.com/shahkushal38/khabrichain" className='btn btn-primary'>Github Repository</a>
+              <a href="https://www.youtube.com/watch?v=oEM4TUQ87kw" className='btn btn-primary'> Project Demo - YouTube </a>
+              <a href="https://devfolio.co/projects/khabrichain-decentralized-reporting-system-7b59" className='btn btn-primary'> Devfolio </a>
+            </div>
             </p>
           </div>
 
